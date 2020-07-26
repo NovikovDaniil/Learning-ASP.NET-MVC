93a796b4f1be7b323981362ee90c3d8b94ade901
Daniil Novikov <novikovdanya2311@mail.ru>
2020-07-26 21:04:27 +0300

    finishing work with the client and server parts

--- MyCompany/Controllers/HomeController.cs
+++ MyCompany/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,20 @@ namespace MyCompany.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;   //для доступа к базе данных
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ MyCompany/Models/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany.Models.ViewComponents
+{
+    public class SideBarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SideBarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+            => Task.FromResult((IViewComponentResult) View
+                ("Default", dataManager.ServiceItems.GetServiceItems()));
+
+
+    }
+}
